333a71f8e55f03a9d3790f5a1471791a392535e3
Mayura Pabasara <mayurapabasara143@gmail.com>
2026-05-12 19:00:35 +0530

    implement OTP email verification system
    - Added OTP model for verification handling
    - Created reusable emailDesigner utility for styled emails
    - Updated user controller with OTP authentication flow
    - Enhanced user routes and synced dependency updates

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,5 +1,13 @@
 import express from "express";
-import {createUser, getUser, getUsers, loginUser} from "../controllers/userController.js";
+import {
+    changePasswordViaOTP,
+    createUser,
+    getUser,
+    getUsers,
+    googleLogin,
+    loginUser,
+    sendOTP
+} from "../controllers/userController.js";
 
 const userRouter = express.Router();
 
@@ -7,5 +15,8 @@ userRouter.get("/", getUsers);
 userRouter.post("/", createUser);
 userRouter.post("/login", loginUser);
 userRouter.get("/me", getUser);
+userRouter.post("/google-login", googleLogin);
+userRouter.get("/send-otp/:email", sendOTP);
+userRouter.post("/change-password/", changePasswordViaOTP);
 
 export default userRouter;
